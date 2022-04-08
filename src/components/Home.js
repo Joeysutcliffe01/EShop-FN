1cf8d2e1e53f52fb19120446733129ab5de1c050
Joey Sutcliffe <joeysutcliffe0@gmail.comgit config --global user.email joeysutcliffe0@gmail.com>
2022-04-08 14:32:31 +0200

    rendering products

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_BASE_URL } from "../consts";
+
 export function Home() {
+  const [products, setProducts] = useState([]);
+  const getAllProducts = () => {
+    axios
+      .get(`${API_BASE_URL}/products`)
+      .then((response) => {
+        setProducts(response.data);
+        console.log(response.data.products);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  console.log(products);
+
+  // try{
+  //   const {data} = await axios.get(`${API_BASE_URL}/products`)
+
+  //   setProducts(data.products)
+  // }catch{}
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
-    <>
+    <div className="main__container">
       {/*---------------------------------------------- Hero  */}
       <section className="hero__section">
         <div className="Hero__container">
@@ -18,6 +45,18 @@ export function Home() {
       {/*---------------------------------------------- Product sliders  */}
       <section className="Product__slider__section">
         <div className="Product__slider__container">
+          <h1>The products should be under me</h1>
+
+          {/* {products.map((product) => {
+            return (
+              <div className="ProjectCard card" key={product._id}>
+                <h3>{product.Title}</h3>
+              </div>
+            );
+          })} */}
+        </div>
+
+        {/* <div className="Product__slider__container">
           <h1>Product slider sections goes here</h1>
         </div>
 
@@ -27,12 +66,8 @@ export function Home() {
 
         <div className="Product__slider__container">
           <h1>Product slider sections goes here</h1>
-        </div>
-
-        <div className="Product__slider__container">
-          <h1>Product slider sections goes here</h1>
-        </div>
+        </div> */}
       </section>
-    </>
+    </div>
   );
 }
