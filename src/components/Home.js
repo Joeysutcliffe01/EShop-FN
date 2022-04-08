bd31814be1e2ae78a1d9d7f7316fdf5a9bf10c4b
Ali Amidpour <ali.amidpour.eng@gmail.com>
2022-04-08 15:36:23 +0200

    push data from db

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 import { API_BASE_URL } from "../consts";
 
 export function Home() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState();
   const getAllProducts = () => {
     axios
       .get(`${API_BASE_URL}/products`)
       .then((response) => {
-        setProducts(response.data);
-        console.log(response.data.products);
+        setProducts(response.data.products);
+       console.log(response.data.products)
       })
       .catch((error) => console.log(error));
   };
@@ -47,13 +47,14 @@ export function Home() {
         <div className="Product__slider__container">
           <h1>The products should be under me</h1>
 
-          {/* {products.map((product) => {
+          {products ? products.map((product) => {
+
             return (
               <div className="ProjectCard card" key={product._id}>
                 <h3>{product.Title}</h3>
               </div>
             );
-          })} */}
+          } ): <p> there is no f data</p>} 
         </div>
 
         {/* <div className="Product__slider__container">
