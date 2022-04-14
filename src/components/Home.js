fab73e6f57b7b1ff9a3dc184637ecf12a0ab61c1
Joey Sutcliffe <joeysutcliffe0@gmail.comgit config --global user.email joeysutcliffe0@gmail.com>
2022-04-14 14:08:43 +0200

    nav add

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,15 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { API_BASE_URL } from "../consts";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProviderWrapper";
 import { Search } from "./Search";
 import "./styling/Home.css";
+import heroImg from "./img/hero-img.jpg";
 // import AddProduct from "../components/FormComponent/CreateForm";
 
 export function Home() {
+  const { user } = useContext(AuthContext);
   const [products, setProducts] = useState();
   const [filterState, setFilterState] = useState("");
 
@@ -28,68 +32,111 @@ export function Home() {
 
   return (
     <div className="main__container">
-
-      <section className="contact-area" id="contact">
-        <div className="text-part"></div>
-      </section>
-
-      <Search filterState={filterState} setFilterState={setFilterState} />
       {/*---------------------------------------------- Hero  */}
-      <section className="hero__section">
-        <div className="Hero__container">
-          <h1>Hero section goes here</h1>
-        </div>
-      </section>
+      {!user ? (
+        <>
+          <section className="hero__section">
+            <div className="Hero__container">
+              {/* <h1>Hero section goes here</h1> */}
+              <img src={heroImg} alt="Hero image" />
+            </div>
+          </section>
+        </>
+      ) : (
+        <>
+          {/*---------------------------------------------- Filter btns  */}
+          <section className="filter__btns__section">
+            <div className="Hero__container">
+              {/* <h1>Filter btns section goes here</h1> */}
+            </div>
+          </section>
 
-      {/*---------------------------------------------- Filter btns  */}
-      <section className="filter__btns__section">
-        <div className="Hero__container">
-          <h1>Filter btns section goes here</h1>
-        </div>
-      </section>
+          {/*---------------------------------------------- Product sliders  */}
+          <Search filterState={filterState} setFilterState={setFilterState} />
+          <section className="Product__slider__section">
+            <div className="Product__slider__container">
+              {/* <h1>The products should be under me</h1> */}
 
-      {/*---------------------------------------------- Product sliders  */}
-      <section className="Product__slider__section">
-        <div className="Product__slider__container">
-          {/* <h1>The products should be under me</h1> */}
+              {/* <AddProduct refreshProduct={getAllProducts} /> */}
 
-          {/* <AddProduct refreshProduct={getAllProducts} /> */}
+              {products ? (
+                products
+                  .filter((product) => {
+                    return product.title
+                      .toLowerCase()
+                      .trim()
+                      .includes(filterState.toLowerCase().trim());
+                  })
+                  .map((product) => {
+                    return (
+                      <div className="product__card" key={product._id}>
+                        <Link to={`/product/${product._id}`}>
+                          <img src={product.imageUrl} alt={product.title} />
+                          <h3>{product.title}</h3>
+                        </Link>
+                      </div>
+                    );
+                  })
+              ) : (
+                <p> there is no data</p>
+              )}
+            </div>
 
-          {products ? (
-            products
-              .filter((product) => {
-                return product.title
-                  .toLowerCase()
-                  .trim()
-                  .includes(filterState.toLowerCase().trim());
-              })
-              .map((product) => {
-                return (
-                  <div className="product__card" key={product._id}>
-                    <Link to={`/product/${product._id}`}>
-                      <img src={product.imageUrl} alt={product.title} />
-                      <h3>{product.title}</h3>
-                    </Link>
-                  </div>
-                );
-              })
-          ) : (
-            <p> there is no data</p>
-          )}
-        </div>
+            {/* <div className="Product__slider__container">
+                  <h1>Product slider sections goes here</h1>
+                </div>
 
-        {/* <div className="Product__slider__container">
-          <h1>Product slider sections goes here</h1>
-        </div>
+                <div className="Product__slider__container">
+                  <h1>Product slider sections goes here</h1>
+                </div>
 
-        <div className="Product__slider__container">
-          <h1>Product slider sections goes here</h1>
-        </div>
+                <div className="Product__slider__container">
+                  <h1>Product slider sections goes here</h1>
+                </div> */}
+          </section>
+          <section className="Product__slider__section">
+            <div className="Product__full__container">
+              {/* <h1>The products should be under me</h1> */}
 
-        <div className="Product__slider__container">
-          <h1>Product slider sections goes here</h1>
-        </div> */}
-      </section>
+              {/* <AddProduct refreshProduct={getAllProducts} /> */}
+
+              {products ? (
+                products
+                  .filter((product) => {
+                    return product.title
+                      .toLowerCase()
+                      .trim()
+                      .includes(filterState.toLowerCase().trim());
+                  })
+                  .map((product) => {
+                    return (
+                      <div className="product__card" key={product._id}>
+                        <Link to={`/product/${product._id}`}>
+                          <img src={product.imageUrl} alt={product.title} />
+                          <h3>{product.title}</h3>
+                        </Link>
+                      </div>
+                    );
+                  })
+              ) : (
+                <p> there is no data</p>
+              )}
+            </div>
+
+            {/* <div className="Product__slider__container">
+                  <h1>Product slider sections goes here</h1>
+                </div>
+
+                <div className="Product__slider__container">
+                  <h1>Product slider sections goes here</h1>
+                </div>
+
+                <div className="Product__slider__container">
+                  <h1>Product slider sections goes here</h1>
+                </div> */}
+          </section>
+        </>
+      )}
     </div>
   );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,66 +1,60 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthProviderWrapper";
-import "./styling/Home.css"
+import "./styling/Home.css";
 
 // add this in later to show and hide things, its the same as if user
 
-export function Layout(logout) {
+export function Layout() {
   const { user } = useContext(AuthContext);
 
   return (
     <>
-        <div className="navbar">
-          Saffron
-        <ul className="nav">
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="#services">Service</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-      <section className="banner-area" id="home"></section>
-
-      <section className="about-area" id="about">
-        <div className="text-part"></div>
-      </section>
-
-      <section className="port-area" id="portfolio">
-        <div className="text-part"></div>
-      </section>
-
-      <section className="service-area" id="services">
-        <div className="text-part">
-          <h1>Services Area</h1>
+      {/* <div className="main__container"> */}
+      <nav class="navbar">
+        <div class="navbar-container container">
+          <input type="checkbox" name="" id="" />
+          <div class="hamburger-lines">
+            <span class="line line1"></span>
+            <span class="line line2"></span>
+            <span class="line line3"></span>
+          </div>
+          <ul class="menu-items">
+            {!user ? (
+              <>
+                <NavLink className="item" to="/about">
+                  About
+                </NavLink>
+                <NavLink className="item" to="/login">
+                  Login
+                </NavLink>
+                <NavLink className="item" to="/register">
+                  Signup
+                </NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink className="item" to="/profile">
+                  Profile
+                </NavLink>
+                <NavLink className="item" to="/cart">
+                  Cart
+                </NavLink>
+                <NavLink className="item" to="/checkout">
+                  Checkout
+                </NavLink>
+                <button>
+                  <a href="#">Logout</a>
+                </button>
+              </>
+            )}
+          </ul>
+          <NavLink className="logo" to="/">
+            E<span>Shop</span>
+          </NavLink>
         </div>
-      </section>
-      <nav className="nav">
-        <NavLink to="/">EShop </NavLink>
-
-        {!user ? (
-          <>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/register">Signup</NavLink>
-          </>
-        ) : (
-          <>
-            <NavLink to="/profile">Profile</NavLink>
-            <NavLink to="/cart">Cart</NavLink>
-            <NavLink to="/checkout">Checkout</NavLink>
-          </>
-        )}
       </nav>
+
       <Outlet />
       <footer className="footer">
         <h1>I am a footer 🙂</h1>
