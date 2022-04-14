3c1a10b261cf4301422f14671cde98bbd73097c4
Ali Amidpour <ali.amidpour.eng@gmail.com>
2022-04-14 14:52:34 +0200

    css homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -56,7 +56,49 @@ export function Home() {
           {/*---------------------------------------------- Filter btns  */}
           <section className="filter__btns__section">
             <div className="Hero__container">
-              {/* <h1>Filter btns section goes here</h1> */}
+            <div class="service">
+		<div className="title">
+			<h2>Our Services</h2>
+		</div>
+
+		<div className="box">
+			<div className="card">
+				<i className="fas fa-bars"></i>
+				<h5>Web Development</h5>
+				<div className="pra">
+					<p>Every website should be built with two primary goals: Firstly, it needs to work across all devices. Secondly, it needs to be fast as possible.</p>
+
+					<p >
+						<a className="button" href="/some/valid/uri">Read More</a>
+					</p>
+				</div>
+			</div>
+
+			<div className="card">
+				<i className="far fa-user"></i>
+				<h5>Web Development</h5>
+				<div className="pra">
+					<p>Every website should be built with two primary goals: Firstly, it needs to work across all devices. Secondly, it needs to be fast as possible.</p>
+
+					<p >
+						<a className="button" href="/some/valid/uri">Read More</a>
+					</p>
+				</div>
+			</div>
+
+			<div className="card">
+				<i className="far fa-bell"></i>
+				<h5>Web Development</h5>
+				<div className="pra">
+					<p>Every website should be built with two primary goals: Firstly, it needs to work across all devices. Secondly, it needs to be fast as possible.</p>
+
+					<p >
+						<a className="button" href="/some/valid/uri">Read More</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
             </div>
           </section>
 
